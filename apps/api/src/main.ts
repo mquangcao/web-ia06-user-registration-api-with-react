f6c017b2697692e88b1a8748dad32f3059702bd7
Quang Cao <mquangcao38@gmail.com>
2025-11-22 00:19:11 +0700

    feat: enable CORS for development and production environments

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:5173', // dev
+      'https://web-ia06-user-registration-api-with.vercel.app', // FE deployed
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
