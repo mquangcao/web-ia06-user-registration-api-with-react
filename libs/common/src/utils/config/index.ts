691bc856b210d44e8861592c1b9f08929852b496
Quang Cao <mquangcao38@gmail.com>
2025-11-23 00:10:50 +0700

    chore(root): update workspaces and turbo task configurations

--- libs/common/src/utils/config/index.ts
+++ libs/common/src/utils/config/index.ts
@@ -1,4 +1,4 @@
-import config from 'config';
+import * as config from 'config';
 
 export function getConfig<T = any>(configPath: string, defaultValue?: T): T {
   return config.has(configPath) ? config.get(configPath) : defaultValue;
