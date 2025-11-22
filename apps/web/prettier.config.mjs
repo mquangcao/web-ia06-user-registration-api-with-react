e16879ffe089fddc12af200a387d40b7ce27289c
Quang Cao <mquangcao38@gmail.com>
2025-11-22 09:41:38 +0700

    feat: add Prettier configuration and sort imports plugin, update .npmrc for legacy peer dependencies

---
+++ apps/web/prettier.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
+const config = {
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: "es5",
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  importOrder: [
+    ".*styles.css$",
+    "",
+    "dayjs",
+    "^react$",
+    "^next$",
+    "^next/.*$",
+    "<BUILTIN_MODULES>",
+    "<THIRD_PARTY_MODULES>",
+    "^@docs/(.*)$",
+    "^@/.*$",
+    "^../(?!.*.css$).*$",
+    "^./(?!.*.css$).*$",
+    "\\.css$",
+  ],
+};
+
+export default config;
